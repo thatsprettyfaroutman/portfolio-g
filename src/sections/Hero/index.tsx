40bf538b5d645d260a73b9b30fb16186e84fcee9
Viljami <viljami.anttonen@gmail.com>
2023-06-19 15:24:56 +0000

    feat: update hero visuals

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -4,46 +4,40 @@ import Image from 'next/image'
 import { usePalette, palette } from '@/styles/theme'
 import { a, useSpringValue } from 'react-spring'
 import * as Styled from './styled'
-import Text from '@/components/Text'
-// TODO: uncomment when Three is functional
-// import Three from '@/three'
-// import Thing from '@/three/components/Thing'
-// import ThingTitle from '@/three/components/ThingTitle'
-// import MouseOrbiter from '@/three/components/MouseOrbiter'
-// import CameraFog from '@/three/components/CameraFog'
+import Three from '@/three'
+import Thing from '@/three/components/Thing'
+import ThingTitle from '@/three/components/ThingTitle'
+import MouseOrbiter from '@/three/components/MouseOrbiter'
 
 const AImage = a(Image)
 
 export default function Hero({ ...restProps }) {
-  const color = usePalette(palette.accent[1])
+  const ambientColor = usePalette(palette.main.fg)
+  const color = usePalette(palette.main.bg)
   const loading = useSpringValue(1)
 
   return (
     <Styled.Hero {...restProps}>
-      <Text.MainHeading>Hi Ghost!</Text.MainHeading>
-      {/* TODO: uncomment when Three is functional */}
-      {/* <Three keepScrollPerspective>
-        <ambientLight color="#fff" />
+      <Three keepScrollPerspective>
+        <ambientLight color={ambientColor} />
         <MouseOrbiter>
-          <Thing
-            position-z={-100}
-            color={color}
-            onFirstRender={() => loading.start(0)}
-          />
-          <ThingTitle scale={[128, 128, 32]}>Hi Ghost!</ThingTitle>
+          <Thing color={color} onFirstRender={() => loading.start(0)} />
+          <ThingTitle position-z={100} scale={[80, 80, 8]}>
+            Hi ghost!
+          </ThingTitle>
         </MouseOrbiter>
-        <CameraFog near={0} far={64} />
-      </Three> */}
+      </Three>
+
       <AImage
         style={{
           opacity: loading,
           // @ts-ignore
           display: loading.to((p) => (p <= 0 ? 'none' : undefined)),
         }}
-        src="/thing-loading-tinified.png"
+        src="/thing-loading.png"
         alt="Ever changing graphical element"
-        width={560}
-        height={524}
+        width={519}
+        height={511}
       />
     </Styled.Hero>
   )
