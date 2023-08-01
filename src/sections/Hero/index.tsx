3a1b904b435953ef82a4c142f4ea470c5e58dcec
Viljami Anttonen <viljami.anttonen@gmail.com>
2023-08-01 17:44:02 +0300

    feat: update hero section to use contentful

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -1,45 +1,25 @@
-import styled from 'styled-components'
+import { PropsWithChildren } from 'react'
+import useIntro from '@/contentful/hooks/useIntro'
 import Three from '@/three'
-import { usePalette, palette } from '@/styles/theme'
-import Text from '@/components/Text'
+import { Heading1 } from '@/components/Text'
 import MouseOrbiter from '@/three/components/MouseOrbiter'
 import AuroraDisc from '@/three/components/AuroraDisc'
 
+import { Wrapper } from './styled'
+
 // TODO: lazy load three stuff
 
-const Wrapper = styled.section`
-  position: relative;
-  display: grid;
-  height: 100vh;
-  min-height: 600px;
-  place-items: center;
-  background: linear-gradient(
-    ${palette.main.background.top},
-    ${palette.main.background.bottom}
-  );
-  user-select: none;
-
-  > .three {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-  }
-`
-
-export default function Hero({ ...restProps }) {
-  const color0 = usePalette(palette.accent[0])
-  const color1 = usePalette(palette.accent[1])
+export default async function Hero(props: PropsWithChildren) {
+  const intro = await useIntro()
 
   return (
-    <Wrapper {...restProps}>
+    <Wrapper {...props}>
       <Three keepScrollPerspective>
         <MouseOrbiter maxAngle={Math.PI * 0.06125}>
-          <AuroraDisc color0={color0} color1={color1} />
+          <AuroraDisc />
         </MouseOrbiter>
       </Three>
-      <Text.Heading1>Hello Ghost</Text.Heading1>
+      <Heading1>{intro.title}</Heading1>
     </Wrapper>
   )
 }
