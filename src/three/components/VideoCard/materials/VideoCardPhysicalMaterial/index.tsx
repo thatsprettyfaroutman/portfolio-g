74361c979e34c4009cd0f2d2b5f925d102e430f3
Viljami Anttonen <viljami.anttonen@gmail.com>
2023-08-21 21:43:44 +0300

    refactor: VideoCard component into multiple components feat: VideoCard flip empty backside material

---
+++ src/three/components/VideoCard/materials/VideoCardPhysicalMaterial/index.tsx
@@ -0,0 +1,127 @@
+import { Color, Texture, Vector2, RepeatWrapping } from 'three'
+import { useEffect, useRef } from 'react'
+import getShaderInjectors from '@/three/utils/injectShader'
+import { useTexture } from '@react-three/drei'
+import { usePalette, palette } from '@/styles/theme'
+import paperNormal from './textures/paper-normal.jpg'
+
+// @ts-ignore
+import fragmentPars from './shaders/pars.frag'
+// @ts-ignore
+import fragmentMain from './shaders/main.frag'
+import { useFrame } from '@react-three/fiber'
+
+export type TVideoCardPhysicalMaterialProps = {
+  map: Texture
+  width: number
+  height: number
+  iconMap?: Texture
+  iconWidth?: number
+  iconHeight?: number
+  attach?: string
+  mouseRef?: React.MutableRefObject<{
+    hover: number
+    position: Vector2
+  }>
+  backside?: boolean
+}
+
+export default function VideoCardPhysicalMaterial({
+  map,
+  width,
+  height,
+  iconMap,
+  iconWidth = 40,
+  iconHeight = 40,
+  mouseRef,
+  backside = false,
+  ...restProps
+}: TVideoCardPhysicalMaterialProps) {
+  const aspect = width / height
+
+  //
+  // Colors
+  const backgroundColor = usePalette(palette.main.background.bottom)
+  const foregroundColor = usePalette(palette.main.text)
+
+  //
+  // Maps
+  const roughnessMap = useTexture(paperNormal.src)
+  roughnessMap.wrapS = RepeatWrapping
+  roughnessMap.wrapT = RepeatWrapping
+  roughnessMap.repeat.x = 4.0
+  roughnessMap.repeat.y = 4.0 / aspect
+
+  //
+  // Uniforms
+  const uniforms = useRef({
+    uResolution: { value: new Vector2(width, height) },
+    uAspect: { value: aspect },
+    uMap: { value: map },
+    uIconMap: { value: new Texture() },
+    uIconMapResolution: { value: new Vector2(iconWidth, iconHeight) },
+    uIconMapColorBackground: { value: new Color(backgroundColor) },
+    uIconMapColorForeground: { value: new Color(foregroundColor) },
+    uMixIcon: { value: !!iconMap },
+    uMouse: { value: new Vector2(0) },
+    uMouseHover: { value: 0 },
+    uFlipMouseY: { value: false },
+    uBackside: { value: backside },
+  })
+
+  useEffect(() => {
+    uniforms.current.uResolution.value.x = width
+    uniforms.current.uResolution.value.y = height
+    uniforms.current.uAspect.value = aspect
+    uniforms.current.uMixIcon.value = !!iconMap
+    if (iconMap) {
+      uniforms.current.uIconMap.value = iconMap
+      uniforms.current.uIconMapResolution.value.x = iconWidth
+      uniforms.current.uIconMapResolution.value.y = iconHeight
+    }
+    uniforms.current.uIconMapColorBackground.value.set(backgroundColor)
+    uniforms.current.uIconMapColorForeground.value.set(foregroundColor)
+    uniforms.current.uBackside.value = backside
+  }, [
+    aspect,
+    backgroundColor,
+    width,
+    height,
+    iconMap,
+    foregroundColor,
+    iconWidth,
+    iconHeight,
+    backside,
+  ])
+
+  useFrame(() => {
+    if (!mouseRef?.current) {
+      return
+    }
+    uniforms.current.uMouse.value.x = mouseRef.current.position.x
+    uniforms.current.uMouse.value.y = mouseRef.current.position.y
+    uniforms.current.uMouseHover.value = mouseRef.current.hover
+  })
+
+  return (
+    <meshPhysicalMaterial
+      map={map}
+      roughnessMap={roughnessMap}
+      onBeforeCompile={(shaderObject) => {
+        shaderObject.uniforms = {
+          ...shaderObject.uniforms,
+          ...uniforms.current,
+        }
+
+        // Inject fragment shader code to specific positions
+        const { fragment } = getShaderInjectors(shaderObject)
+        fragment('#include <clipping_planes_pars_fragment>', fragmentPars)
+        fragment('#include <map_fragment>', fragmentMain)
+      }}
+      {...restProps}
+    />
+  )
+}
+
+// Preload textures
+useTexture.preload(paperNormal.src)
--- src/three/components/VideoCard/materials/VideoCardPhysicalMaterial/shaders/main.frag
+++ src/three/components/VideoCard/materials/VideoCardPhysicalMaterial/shaders/main.frag
@@ -3,6 +3,9 @@
 
 diffuseColor = getDiffuseColor(vMapUv);
 diffuseColor = mixMouseBlob(diffuseColor);
+
+if(uMixIcon) {
 diffuseColor = mixIcon(diffuseColor, vMapUv);
+}
 
 #endif
