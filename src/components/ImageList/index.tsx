2e66148fd68f4f4c196d0c3c74b5f161bf829e05
Viljami Anttonen <viljami.anttonen@gmail.com>
2023-08-31 12:15:46 +0300

    refactor: move ImageList styles and logic into own files

--- src/components/ImageList/index.tsx
+++ src/components/ImageList/index.tsx
@@ -5,152 +5,29 @@ import { useTransition, a } from 'react-spring'
 import styled from 'styled-components'
 import { TImageList } from '@/contentful/types'
 import usePrefetchImage from '@/hooks/usePrefetchImage'
-import { palette } from '@/styles/theme'
 import MorphyIcon from './components/MorphyIcon'
+import useImageList from './hooks/useImageList'
+import { Wrapper, Items, OpenImage, Shade, Controls } from './styled'
 
 type TImageListProps = {
   children: TImageList
 }
-
-const Wrapper = styled.div``
-
-const Items = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-  gap: calc(var(--space) / 4);
-
-  > a {
-    position: relative;
-    display: inline-block;
-
-    ::before {
-      content: ' ';
-      position: absolute;
-      top: calc(var(--space) / 16);
-      left: calc(var(--space) / 16);
-      width: 100%;
-      height: 100%;
-      border: 1px solid ${palette.main.text};
-      border-top: none;
-      border-left: none;
-    }
-
-    > img {
-      position: relative;
-      display: block;
-      margin: 0;
-    }
-  }
-`
-
-const OpenImage = styled.div`
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 1;
-  pointer-events: none;
-  user-select: none;
-
-  > img {
-    position: absolute;
-    top: calc(var(--space) / 2);
-    left: calc(var(--space) / 2);
-    width: calc(100% - var(--space));
-    height: calc(100% - var(--space));
-    object-fit: contain;
-  }
-`
-
-const Shade = styled.div`
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  background-color: #0004;
-  z-index: 1;
-  user-select: none;
-`
-
-const Controls = styled.div`
-  position: fixed;
-  top: 50%;
-  height: var(--space);
-  right: 0;
-  left: 0;
-  transform: translateY(-50%);
-  z-index: 1;
-  user-select: none;
-
-  > div {
-    position: absolute;
-    width: var(--space);
-    height: 100%;
-    top: 50%;
-    left: 0;
-    cursor: pointer;
-    background-color: ${palette.main.background.top};
-
-    :last-child {
-      left: auto;
-      right: 0;
-    }
-  }
-`
 
 const AShade = a(Shade)
 const AOpenImage = a(OpenImage)
 const AControls = a(Controls)
 
 function ImageList({ children, ...restProps }: TImageListProps) {
-  const [openIndex, setOpenIndex] = useState(-1)
-  const { prefetchImage, bindPrefetchImage } = usePrefetchImage()
-
-  const openImage = children.images.items[openIndex]
-  const prevImage =
-    openIndex >= 0 ? children.images.items[openIndex - 1] : undefined
-  const nextImage =
-    openIndex >= 0 ? children.images.items[openIndex + 1] : undefined
-  prevImage && prefetchImage(prevImage.url)
-  nextImage && prefetchImage(nextImage.url)
-
-  const shadeTransitions = useTransition(openIndex > -1, {
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
+  const {
+    bindPrefetchImage,
+    handleOpenImage,
+    handleChangeImage,
+    handleCloseImage,
+    shadeTransitions,
+    imageTransitions,
+  } = useImageList({
+    children,
   })
-
-  const imageTransitions = useTransition(openImage, {
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
-  })
-
-  const directionRef = useRef(0)
-
-  const handleOpenImage = (index: number) => (e) => {
-    e.preventDefault()
-    directionRef.current = 0
-    setOpenIndex(index)
-  }
-
-  const handleNextImage = (direction: -1 | 1) => () => {
-    directionRef.current = direction
-    setOpenIndex((s) => {
-      const next = s + direction
-      if (next < 0 || next >= children.images.items.length) {
-        return -1
-      }
-      return next
-    })
-  }
-
-  const handleCloseImage = () => {
-    directionRef.current = 0
-    setOpenIndex(-1)
-  }
 
   return (
     <Wrapper {...restProps}>
---
+++ src/components/ImageList/styled.ts
@@ -0,0 +1,91 @@
+import styled from 'styled-components'
+import { palette } from '@/styles/theme'
+
+export const Wrapper = styled.div``
+
+export const Items = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  gap: calc(var(--space) / 4);
+
+  > a {
+    position: relative;
+    display: inline-block;
+
+    ::before {
+      content: ' ';
+      position: absolute;
+      top: calc(var(--space) / 16);
+      left: calc(var(--space) / 16);
+      width: 100%;
+      height: 100%;
+      border: 1px solid ${palette.main.text};
+      border-top: none;
+      border-left: none;
+    }
+
+    > img {
+      position: relative;
+      display: block;
+      margin: 0;
+    }
+  }
+`
+
+export const OpenImage = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1;
+  pointer-events: none;
+  user-select: none;
+
+  > img {
+    position: absolute;
+    top: calc(var(--space) / 2);
+    left: calc(var(--space) / 2);
+    width: calc(100% - var(--space));
+    height: calc(100% - var(--space));
+    object-fit: contain;
+  }
+`
+
+export const Shade = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  /* TODO: theme color */
+  background-color: #0004;
+  z-index: 1;
+  user-select: none;
+`
+
+export const Controls = styled.div`
+  position: fixed;
+  top: 50%;
+  height: var(--space);
+  right: 0;
+  left: 0;
+  transform: translateY(-50%);
+  z-index: 1;
+  user-select: none;
+
+  > div {
+    position: absolute;
+    width: var(--space);
+    height: 100%;
+    top: 50%;
+    left: 0;
+    cursor: pointer;
+    background-color: ${palette.main.background.top};
+
+    :last-child {
+      left: auto;
+      right: 0;
+    }
+  }
+`
