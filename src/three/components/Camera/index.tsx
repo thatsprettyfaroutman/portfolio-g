fc9d4e5dd72ecc10d2894e1f65cf324564fda584
Viljami Anttonen <viljami.anttonen@gmail.com>
2023-09-11 20:59:52 +0300

    docs: explain custom camera

--- src/three/components/Camera/index.tsx
+++ src/three/components/Camera/index.tsx
@@ -2,6 +2,10 @@ import { useRef } from 'react'
 import { PerspectiveCamera } from '@react-three/drei'
 import { useThree } from '@react-three/fiber'
 
+// Perspective camera that mimics Css's camera.
+// It has the same the pixel ratio as the rest of the site, making it easier to position and scale 3d elements so they match rest of the site.
+// 100 3d-measurement-units === 100px
+
 // Inspired by this thread https://discourse.threejs.org/t/solved-how-do-we-size-something-using-screen-pixels/1177/6
 
 type TCameraProps = {
