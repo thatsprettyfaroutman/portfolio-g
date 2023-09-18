65f728bf17d2e8c4f7dbafbf2d7ff3527ad94c2e
Viljami Anttonen <viljami.anttonen@gmail.com>
2023-09-18 22:31:19 +0300

    feat: home page to have actual portfolio content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
-import Dancer from '@/three/components/Dancer/lazy'
+import Footer from '@/asyncComponents/Footer'
+import IntroSection from '@/asyncComponents/IntroSection'
+import SocialMediaLinks from '@/asyncComponents/SocialMediaLinks'
+import WorkSection from '@/asyncComponents/WorkSection'
+import Header from '@/components/Header'
+import Logo from '@/components/Logo'
+import Main from '@/components/Main'
 import Favicon from '@/three/components/Favicon/lazy'
-import Three from '@/three/lazy'
-import { GlobalStyle } from './styled'
 
 export default async function Home() {
   return (
-    <>
-      <GlobalStyle />
+    <Main>
       <Favicon />
-      <Three keepDefaultCamera>
-        <Dancer />
-      </Three>
-    </>
+      <Header>
+        <Logo>Viljami.dev</Logo>
+        <SocialMediaLinks stealthMode />
+      </Header>
+      <IntroSection />
+      <WorkSection />
+      <Footer />
+    </Main>
   )
 }
