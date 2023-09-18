eb30504cdc1a19613336c95032164d1b66b51501
Viljami Anttonen <viljami.anttonen@gmail.com>
2023-09-18 22:30:12 +0300

    fix: buggy Marquee loadings layout

--- src/components/Marquee/index.tsx
+++ src/components/Marquee/index.tsx
@@ -3,7 +3,7 @@
 import { PropsWithChildren } from 'react'
 import range from 'ramda/src/range'
 import useMeasure from 'react-use-measure'
-import styled, { keyframes } from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 
 type TMarqueeProps = PropsWithChildren
 
@@ -13,17 +13,22 @@ const Wrapper = styled.div`
   overflow: hidden;
 `
 
-const Slider = styled.div`
+const Slider = styled.div<{ $active: boolean }>`
   position: absolute;
   top: 50%;
   left: 0;
-  animation-name: ${keyframes`
-    0% { transform: translate3d(0%, -50%, 0)}
-    100% { transform: translate3d(-100%, -50%, 0)}
+
+  ${(p) =>
+    p.$active &&
+    css`
+      animation-name: ${keyframes`
+    0% { transform: translate3d(0%, 0, 0)}
+    100% { transform: translate3d(-100%, 0, 0)}
   `};
-  animation-timing-function: linear;
-  animation-duration: 20s;
-  animation-iteration-count: infinite;
+      animation-timing-function: linear;
+      animation-duration: 20s;
+      animation-iteration-count: infinite;
+    `};
 `
 
 const Content = styled.div`
@@ -35,7 +40,11 @@ const Content = styled.div`
 export default function Marquee({ children, ...restProps }: TMarqueeProps) {
   const [wrapperRef, wrapperBounds] = useMeasure()
   const [contentRef, contentBounds] = useMeasure()
-  const numContentsFit = Math.ceil(wrapperBounds.width / contentBounds.width)
+  const numContentsFit = contentBounds.width
+    ? Math.ceil(wrapperBounds.width / contentBounds.width) + 1
+    : 4
+
+  const ready = !!contentBounds.width
 
   return (
     <Wrapper
@@ -44,17 +53,18 @@ export default function Marquee({ children, ...restProps }: TMarqueeProps) {
       style={{ height: contentBounds.height }}
     >
       <Slider
+        $active={ready}
         style={{
           width: contentBounds.width,
           height: contentBounds.height,
         }}
       >
-        <Content ref={contentRef}>{children}</Content>
         {range(0, numContentsFit).map((i) => (
           <Content
+            ref={i === 0 ? contentRef : null}
             key={i}
             style={{
-              left: contentBounds.width * (i + 1),
+              transform: `translate(${i * 100}%, -50%)`,
             }}
           >
             {children}
