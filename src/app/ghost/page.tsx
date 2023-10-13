54e9e2cfdbe96ca9473248934010b01d0966ed8b
Viljami Anttonen <viljami.anttonen@gmail.com>
2023-10-13 17:53:51 +0900

    chore: update routes

--- src/app/ghost/page.tsx
+++ src/app/ghost/page.tsx
@@ -1,6 +1,6 @@
 import Footer from '@/asyncComponents/Footer'
-import HeroGhost from '@/asyncComponents/HeroGhost'
 import IntroSection from '@/asyncComponents/IntroSection'
+import ProfileDrawer from '@/asyncComponents/ProfileDrawer'
 import SocialMediaLinks from '@/asyncComponents/SocialMediaLinks'
 import WorkSection from '@/asyncComponents/WorkSection'
 import Header from '@/components/Header'
@@ -11,13 +11,13 @@ export default async function Home() {
   return (
     <Main>
       <Header absolute>
-        <Logo>/ Ghost</Logo>
+        <Logo />
         <SocialMediaLinks stealthMode />
       </Header>
-      <HeroGhost title="Hello Ghost" />
       <IntroSection />
       <WorkSection />
       <Footer />
+      <ProfileDrawer />
     </Main>
   )
 }
